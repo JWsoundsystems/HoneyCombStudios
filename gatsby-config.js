f237bb10d166855d031c307c181608e44f49b2a6
Jeffrey Walsh <marwal@Jeffreys-iMac-Pro-4.local>
2020-06-22 07:53:40 -0700

    organized image assets, added H&C icon to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Honey & Comb Salon`,
+    description: `WHETHER YOU’RE SEEKING A NEW STYLE, COLOR OR CUT, HONEY & COMB SALON WELCOMES EACH CLIENT AS AN OLD FRIEND...AND WE PROVIDE EXPERTISE TAILORED TO YOUR HAIR TYPE.  WE STAY ON TOP OF THE CURRENT HAIR TRENDS WITH OUR CONTINUING EDUCATION, ALL TO ENSURE WE DELIVER THE MOST BEAUTIFUL HAIR. LET US MAKE YOUR LIFE SWEETER BY VISITING HONEY & COMB SALON TODAY AND FIND OUT WHAT THE BUZZ IS ALL ABOUT.`,
+    author: `@jvelvetdisco`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Logo/02_Logo.jpg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
